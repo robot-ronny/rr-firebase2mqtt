bc483a4626cfc3d9920a50ba06f3756cb32a26c6
worepix <hry-kurasa@seznam.cz>
2019-04-05 15:04:51 +0200

    Add app

---
+++ app.js
@@ -0,0 +1,37 @@
+var admin = require("firebase-admin");
+var mqtt = require("mqtt");
+var program = require("commander");
+
+program
+  .version('0.1.0')
+  .option('-f, --firebase <url>', 'Firebase url')
+  .option('-c, --cert <cert>', 'serviceAccountKey path')
+  .option('-h, --host <host>', 'MQTT broker host', defaultValue="localhost")
+  .option('-p, --port <port>', 'MQTT broker port', defaultValue=1883)
+  .parse(process.argv);
+
+if ((program.firebase && program.cert) == undefined) {
+    console.log(" ==> Error: Enter firebase url and serviceAccountKey path")
+    process.exit()
+}
+
+var client  = mqtt.connect('mqtt://{program.host}:{program.port}')
+ 
+client.on('connect', function () {
+  console.log("Connected to MQTT broker")
+})
+
+admin.initializeApp({
+  credential: admin.credential.cert(program.cert),
+  databaseURL: program.firebase
+});
+
+var ref = admin.database().ref("/commands/go");
+
+ref.on("value", snapshot => {
+    var recieved_data = snapshot.val();
+
+    var message = JSON.stringify({"interval": parseInt(recieved_data.value), "speed": 100});
+    client.publish("ronny/go/" + recieved_data.direction, message);
+    console.log("Message send to MQTT broker " + message)
+})
